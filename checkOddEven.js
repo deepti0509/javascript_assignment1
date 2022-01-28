28d028c37ad0e56556d1ce36c3455992659f3197
Deepti Mehra <mehradeepti0509@gmail.com>
2022-01-28 13:29:01 +0530

    add checkOddEven.js

---
+++ checkOddEven.js
@@ -0,0 +1,13 @@
+function checkOddEven(number) {
+    const remainder = number % 2;
+    if (remainder == 0) {
+        console.log("number is even")
+    } else {
+        console.log("number is odd")
+
+    }
+} 
+
+
+checkOddEven(20)
+checkOddEven(21)
