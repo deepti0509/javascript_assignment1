c044442cac209e80ff5d6a5ccdf16476247ccb7f
Deepti Mehra <mehradeepti0509@gmail.com>
2022-01-28 13:18:14 +0530

    add a file to check if value is zero or negative or positive

---
+++ zeroPositiveNegativeTest.js
@@ -0,0 +1,18 @@
+function zeroPositiveNegativeTest(number) {
+    if (number == 0) {
+        console.log("Number is zero")
+
+    } else if (number < 0) {
+
+        console.log("Number is negative");
+
+    } else {
+
+        console.log("Number is positive")
+    }
+
+}
+
+zeroPositiveNegativeTest(0);
+zeroPositiveNegativeTest(10);
+zeroPositiveNegativeTest(-10);
